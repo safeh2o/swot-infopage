0436232e911cf995e119c405a70c6e805677f025
Mohamed Moselhy <look4d@gmail.com>
2022-03-16 17:48:39 -0400

    converted to html

--- includes/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="site-footer">
-  <p>© Safe Water Optimization Tool at the Dahdaleh Institute for Global Health Research. All rights reserved.</p>
-  <div><a href="#">Back to Top</a></div>
-</footer>
